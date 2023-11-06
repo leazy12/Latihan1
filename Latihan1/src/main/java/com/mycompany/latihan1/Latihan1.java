d371f93a09853efbee702cd838ce08611ea9d53f
leazy <101962294+leazy12@users.noreply.github.com>
2023-11-06 22:51:35 +0800

    Add files via upload

---
+++ Latihan1/src/main/java/com/mycompany/latihan1/Latihan1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author mkhai
+ */
+public class Latihan1 {
+
+    public static void main(String[] args) {
+        System.out.println("Muhammad Miftah Khairiyan");
+        System.out.println("5B Reg Pagi BJB");
+        System.out.println("2110010035");
+    }
+}
